d4d59211de08ce5034b3b5a6bf729ca8097167ad
Justino Soares <justinocsoares123@gmail.com>
2026-04-20 07:53:17 +0100

    feat: finiesh

--- app/main.py
+++ app/main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from app.modules.documents.routes import router as doc_router
 from app.modules.rag.routes import router as rag_router
 from app.core.errors import register_error_handlers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(doc_router)
 app.include_router(rag_router)
